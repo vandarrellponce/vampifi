f4ca86a5540c532b2376ef53341c65b7eefae2c4
Van Darrell Ponce <riversideriver@yahoo.com>
2020-11-26 17:52:19 +0800

    added category list screen, modal and initial function to add category

--- frontend/admin-app/src/containers/CategoryListScreen/CategoryListSceen.tsx
+++ frontend/admin-app/src/containers/CategoryListScreen/CategoryListSceen.tsx
@@ -1,14 +1,20 @@
 import React, { useEffect, useState } from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
+import { Button, Col, Container, Modal, Row } from 'react-bootstrap'
 import Layout from '../../components/Layout/Layout'
 import { useDispatch, useSelector } from 'react-redux'
 import './CategoryListSceen.css'
 import getCategories from '../../store/actions/category.getCategories'
 import Message from '../../components/Message/Message'
 import Loader from '../../components/Loader/Loader'
+import Input from '../../components/UI/Input/Input'
 
 const CategoryListSceen = () => {
   const [loading, setLoading] = useState(false)
+  const [showModal, setShowModal] = useState(false)
+
+  const [newCatName, setNewCatName] = useState('')
+  const [newCatParent, setNewCatParent] = useState('')
+  const [newCatImage, setNewCatImage] = useState('')
 
   const { categoryList, categoryListError } = useSelector(
     (state) => state.category
@@ -21,7 +27,53 @@ const CategoryListSceen = () => {
       dispatch(getCategories()).then((_) => setLoading(false))
     }
     /* eslint-disable */
-  }, [dispatch])
+  }, [dispatch, categoryList])
+
+  // FUNCTIONS & HANDLERS
+  const renderCategories = (categories) => {
+    return categories.map((cat) => {
+      return (
+        <li key={cat._id}>
+          {cat.name}
+          {cat.children.length > 0 && <ul>{renderCategories(cat.children)}</ul>}
+        </li>
+      )
+    })
+  }
+
+  const listCategoryOptions = (categories, options = []) => {
+    categories.forEach((cat) => {
+      options.push({ value: cat._id, name: cat.name })
+      if (cat.children.length) {
+        listCategoryOptions(cat.children, options)
+      }
+    })
+
+    return options
+  }
+
+  const handleClose = () => {
+    const form = new FormData()
+    const newCat = {
+      name: newCatName,
+      parentId: newCatParent,
+      imageUrl: newCatImage
+    }
+
+    console.log(newCat)
+  }
+
+  const toggleModal = () => {
+    setShowModal((prevState) => !prevState)
+  }
+
+  const handleImage = (e) => {
+    setNewCatImage(e.target.files[0])
+  }
+
+  const handleChange = (e) => {
+    setNewCatParent(e.currentTarget.value)
+  }
 
   return (
     <Layout showSidebar>
@@ -32,14 +84,62 @@ const CategoryListSceen = () => {
           <Col md={12}>
             <div className="categorylist__col__one">
               <h2>Categories</h2>
-              <button type="button" className="my-3 btn btn-dark">
+              <button
+                type="button"
+                className="my-3 btn btn-dark"
+                onClick={toggleModal}
+              >
                 Add Category
               </button>
             </div>
           </Col>
-          <Col md={12}></Col>
+          <Col md={12}>
+            <ul>{categoryList && renderCategories(categoryList)}</ul>
+          </Col>
         </Row>
       </Container>
+
+      <Modal show={showModal} onHide={toggleModal}>
+        <Modal.Header>
+          <Modal.Title>Add new Category</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Input
+            type="text"
+            label="Category Name"
+            placeholder="Enter category name"
+            required={true}
+            value={newCatName}
+            onChange={setNewCatName}
+          />
+          Parent Category <br />
+          <select
+            className="form-control"
+            value={newCatParent}
+            onChange={handleChange}
+          >
+            <option>select category</option>
+            {categoryList?.length > 0 &&
+              listCategoryOptions(categoryList).map((option) => {
+                return (
+                  <option value={option.value} key={option.value}>
+                    {option.name}
+                  </option>
+                )
+              })}
+          </select>
+          <input type="file" name="categoryImage" onChange={handleImage} />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+
+          <Button variant="dark" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Layout>
   )
 }
