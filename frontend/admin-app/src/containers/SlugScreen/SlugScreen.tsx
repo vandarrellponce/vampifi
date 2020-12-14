bfea2b4e4286c174c203787feb10b1110f4f80ce
Van Darrell Ponce <riversideriver@yahoo.com>
2020-12-14 21:21:10 +0800

    logic for extracting query params

--- frontend/admin-app/src/containers/SlugScreen/SlugScreen.tsx
+++ frontend/admin-app/src/containers/SlugScreen/SlugScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ClientLayout from '../../components/LayoutClient/ClientLayout'
 
 import './SlugScreen.css'
@@ -6,6 +6,11 @@ import './SlugScreen.css'
 import StoreDisplay from './StoreDisplay/StoreDisplay'
 
 const SlugScreen = (props) => {
+  const queries = props.location.search.replace('?', '').split('&')
+  const categoryId = queries[0].split('=')[1]
+  const displayType = queries[1].split('=')[1]
+  console.log({ categoryId, displayType })
+
   return (
     <ClientLayout>
       <StoreDisplay {...props} />
