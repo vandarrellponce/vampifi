766e44c25451641db84ceacb636725a3f37a777b
Van Darrell Ponce <riversideriver@yahoo.com>
2020-12-15 18:52:45 +0800

    initial creation of PageDisplay

---
+++ frontend/admin-app/src/containers/SlugScreen/PageDisplay/PageDisplay.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './PageDisplay.css'
+
+const PageDisplay = () => {
+  return <div>Page Display Component</div>
+}
+
+export default PageDisplay
--- frontend/admin-app/src/containers/SlugScreen/SlugScreen.tsx
+++ frontend/admin-app/src/containers/SlugScreen/SlugScreen.tsx
@@ -1,21 +1,27 @@
 import React, { useState } from 'react'
 import ClientLayout from '../../components/LayoutClient/ClientLayout'
+import PageDisplay from './PageDisplay/PageDisplay'
 
 import './SlugScreen.css'
 
 import StoreDisplay from './StoreDisplay/StoreDisplay'
 
 const SlugScreen = (props) => {
-  const queries = props.location.search.replace('?', '').split('&')
-  const categoryId = queries[0].split('=')[1]
-  const displayType = queries[1].split('=')[1]
-  console.log({ categoryId, displayType })
+  const renderProduct = () => {
+    const queries = props.location.search.replace('?', '').split('&')
+    const categoryId = queries[0].split('=')[1]
+    const displayType = queries[1].split('=')[1]
+    switch (displayType) {
+      case 'store':
+        return <StoreDisplay {...props} />
+      case 'page':
+        return <PageDisplay {...props} />
+      default:
+        return <div>PAGE NOT FOUND</div>
+    }
+  }
 
-  return (
-    <ClientLayout>
-      <StoreDisplay {...props} />
-    </ClientLayout>
-  )
+  return <ClientLayout>{renderProduct()}</ClientLayout>
 }
 
 export default SlugScreen
