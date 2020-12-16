62dc36a7ca36b2d88bad457ad62e69bb4f56b64c
Van Darrell Ponce <riversideriver@yahoo.com>
2020-12-16 16:24:11 +0800

    get page action

--- frontend/admin-app/src/store/actions/page.getPage.ts
+++ frontend/admin-app/src/store/actions/page.getPage.ts
@@ -1,12 +1,14 @@
 import axiosHelper from '../../helpers/axios'
 import defineError from '../../helpers/defineError'
+import { PAGE_GET_FAIL, PAGE_GET_SUCCESS } from '../constants/page'
 
 const getPage = ({ category, type }) => async (dispatch) => {
   try {
     const page = await (await axiosHelper.get(`/page/${category}/${type}`)).data
+    dispatch({ type: PAGE_GET_SUCCESS, payload: page })
     return page
   } catch (error) {
-    return defineError(error)
+    dispatch({ type: PAGE_GET_FAIL, payload: defineError(error) })
   }
 }
 
