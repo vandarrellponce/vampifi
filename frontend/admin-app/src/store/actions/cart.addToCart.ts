b76996c3f54ec713059081ca19c9d12b37da51f2
Van Darrell Ponce <riversideriver@yahoo.com>
2021-01-21 11:09:14 +0800

    added addToCart action

---
+++ frontend/admin-app/src/store/actions/cart.addToCart.ts
@@ -0,0 +1,26 @@
+import axiosHelper from '../../helpers/axios'
+import { CART_ADD_ITEM } from '../constants/cart'
+
+const addToCart = (id, qty) => async (dispatch, getState) => {
+  try {
+    const product = await (await axiosHelper.get(`/products/${id}`)).data
+    const item = {
+      name: product.name,
+      qty: qty,
+      image: product.images[0],
+      price: product.price,
+      countInStock: product.countInStock,
+      productId: product._id
+    }
+    // DISPATCH AND SAVE TO LOCAL STORAGE
+    dispatch({ type: CART_ADD_ITEM, payload: item })
+    localStorage.setItem(
+      'flipkart_cartItems',
+      JSON.stringify(getState().cart.cartItems)
+    )
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export default addToCart
