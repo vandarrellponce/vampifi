64c8bc094eef1828772aac363a398b2de9360f32
Van Darrell Ponce <riversideriver@yahoo.com>
2021-01-21 11:51:47 +0800

    added cartReducer

---
+++ frontend/admin-app/src/store/reducers/cartReducer.ts
@@ -0,0 +1,58 @@
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_PAYMENT_METHOD
+} from '../constants/cart'
+
+const cartReducer = (state = { cartItems: [] }, action) => {
+  switch (action.type) {
+    case CART_ADD_ITEM: {
+      const newItem = action.payload
+
+      // if newItem is already in the cartItems, then replace the old item with the new one
+      // product in here is the product id
+      const itemExists = state.cartItems.find(
+        (currentItem) => currentItem.product === newItem.product
+      )
+      if (itemExists) {
+        return {
+          ...state,
+          cartItems: [...state.cartItems].map((currentItem) =>
+            currentItem.product === newItem.product ? newItem : currentItem
+          )
+        }
+      }
+
+      return {
+        ...state,
+        cartItems: [...state.cartItems, newItem]
+      }
+    }
+    case CART_REMOVE_ITEM: {
+      return {
+        ...state,
+        cartItems: [...state.cartItems].filter(
+          (item) => item.product !== action.payload
+        )
+      }
+    }
+    case CART_SAVE_PAYMENT_METHOD: {
+      return {
+        ...state,
+        paymentMethod: action.payload
+      }
+    }
+
+    case 'CART_RESET': {
+      return {
+        ...state,
+        cartItems: []
+      }
+    }
+
+    default:
+      return state
+  }
+}
+
+export default cartReducer
--- frontend/admin-app/src/store/store.ts
+++ frontend/admin-app/src/store/store.ts
@@ -5,15 +5,26 @@ import userReducer from './reducers/userReducer'
 import categoryReducer from './reducers/categoryReducer'
 import productReducer from './reducers/productReducer'
 import pageReducer from './reducers/pageReducer'
+import cartReducer from './reducers/cartReducer'
+
+// RETREIVING ITEMS FROM STORAGE
+const cartItemsFromStorage = localStorage.getItem('flipkart_cartItems')
+  ? JSON.parse(localStorage.getItem('flipkart_cartItems'))
+  : []
 
 // INITIAL STATE
-const initialState = {}
+const initialState = {
+  cart: {
+    cartItems: cartItemsFromStorage
+  }
+}
 // ROOT REDUCER
 const rootReducer = combineReducers({
   user: userReducer,
   category: categoryReducer,
   product: productReducer,
-  page: pageReducer
+  page: pageReducer,
+  cart: cartReducer
 })
 // STORE
 const middleware = [thunk]
